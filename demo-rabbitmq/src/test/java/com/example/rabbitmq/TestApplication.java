12cd13280b82dd4ea197e6c214c23ad1249012e3
chenzq <chenzq2@allinpay.com>
2023-05-17 17:50:33 +0800

    持久化

--- demo-rabbitmq/src/test/java/com/example/rabbitmq/TestApplication.java
+++ demo-rabbitmq/src/test/java/com/example/rabbitmq/TestApplication.java
@@ -5,10 +5,13 @@ import com.rabbitmq.client.BuiltinExchangeType;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.GetResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @author chenzhiqin
@@ -25,8 +28,9 @@ public class TestApplication {
     }
 
     public static void main(String[] args) throws Exception {
-        publish();
+//        publish();
 //        consumer();
+        pullOne();
     }
 
 
@@ -37,19 +41,13 @@ public class TestApplication {
      * @see RabbitTemplateConfig#txTemplate()
      */
     public static void publish() throws Exception {
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("192.168.75.129");
-        factory.setPort(5672);
-        factory.setUsername("rabbit");
-        factory.setPassword("123456");
-        Connection connection = factory.newConnection();
-        Channel channel = connection.createChannel();
+        Channel channel = createChannel();
         channel.txSelect();
         try {
             channel.exchangeDeclare("direct", BuiltinExchangeType.DIRECT);
             channel.queueDeclare("direct", false, false, false, null);
             channel.queueBind("direct", "direct", "direct");
-            String message = "hello";
+            String message = "hello" + ":" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd HH:mm"));
             channel.basicPublish("direct", "direct", null, message.getBytes(StandardCharsets.UTF_8));
             channel.txCommit();
         } catch (Exception e) {
@@ -60,17 +58,40 @@ public class TestApplication {
     }
 
 
-    public static void consumer() throws Exception {
+    public static Channel createChannel() throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("192.168.75.129");
         factory.setPort(5672);
         factory.setUsername("rabbit");
         factory.setPassword("123456");
         Connection connection = factory.newConnection();
-        Channel channel = connection.createChannel();
+        return connection.createChannel();
+    }
+
+
+    /**
+     * 订阅消费
+     *
+     * @throws Exception
+     */
+    public static void consumer() throws Exception {
+        Channel channel = createChannel();
         channel.basicConsume("direct", true, (a, b) -> {
             System.out.println("收到消息" + new String(b.getBody()));
         }, (a, b) -> {
         });
+    }
+
+
+    /**
+     * 每次只拉取一条消息
+     *
+     * @throws Exception
+     */
+    public static void pullOne() throws Exception {
+        Channel channel = createChannel();
+        GetResponse response = channel.basicGet("direct", true);
+        System.out.println("收到消息:" + new String(response.getBody()));
+
     }
 }
