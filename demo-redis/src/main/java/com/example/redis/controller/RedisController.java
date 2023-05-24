79dd654c4f5475531a432279ba254b5651f7c89c
chenzq <chenzq2@allinpay.com>
2023-05-24 14:37:22 +0800

    命令追加

---
+++ demo-redis/src/main/java/com/example/redis/controller/RedisController.java
@@ -0,0 +1,13 @@
+package com.example.redis.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author chenzhiqin
+ * @date 23/5/2023 15:18
+ * @info 入口
+ */
+@Controller
+public class RedisController {
+}
