615f02eabb9eef482b3a805141f054b81c2a6cb4
chenzq <chenzq2@allinpay.com>
2024-04-18 10:51:10 +0800

    测试5

--- userservice/src/main/java/com/user/UserApp.java
+++ userservice/src/main/java/com/user/UserApp.java
@@ -11,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserApp {
     public static void main(String[] args) {
+        System.out.println("更新1");
         System.out.println("push 1");
         SpringApplication.run(UserApp.class, args);
     }
