fbda4ef3b90d77ebe6880522b3c7a7ca4ad716f4
chenzq <chenzq2@allinpay.com>
2024-04-18 17:14:22 +0800

    Revert "测试5"
    This reverts commit 615f02eabb9eef482b3a805141f054b81c2a6cb4.

--- userservice/src/main/java/com/user/UserApp.java
+++ userservice/src/main/java/com/user/UserApp.java
@@ -11,7 +11,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserApp {
     public static void main(String[] args) {
-        System.out.println("更新1");
         System.out.println("push 1");
         SpringApplication.run(UserApp.class, args);
     }
